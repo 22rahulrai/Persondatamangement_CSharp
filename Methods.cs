7dd643dddbd085e1164d36181f9de95502ca7aaf
Rahul Rai <rairayrahul22@gmail.com>
2025-04-01 18:33:29 +0530

    initial commit

---
+++ Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace review1april
+{
+    public interface  Methods
+    {
+        public void Add(string name,int age,int salay);
+        public void Search(string name);
+        public void Sort();
+        public void Sortbynameage();
+        public void Printdata();
+    }
+}
