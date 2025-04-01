3e0d683030959fce6bcca6c4511e4de2e23449f3
Rahul Rai <rairayrahul22@gmail.com>
2025-04-01 18:33:01 +0530

    initial commit

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace review1april
+{
+    public class Person
+    {
+        public  string Name { get; set; }
+        public  int Age { get; set; }
+        public int Salary { get; set; }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + " Age: " + Age + " Salary: " + Salary;
+        }
+
+    }
+
+    
+
+}
